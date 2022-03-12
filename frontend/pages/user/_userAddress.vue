51910147573553f65d9fac5703e51637d21f2e34
Kair0s3 <stianboon@gmail.com>
2022-03-12 10:41:39 +0800

    Add pages for the planned routing
    1. /campaign/_campaignAddress/info.vue
    2. /campaign/_campaignAddress/edit.vue
    3. /campaign/add
    4. /user/_userAddress.vue
    
    Sample Test
    - localhost:3000/campaign/123/info
    - localhost:3000/campaign/123/edit
    - localhost:3000/campaign/add
    - localhost:3000/user/456

---
+++ frontend/pages/user/_userAddress.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <!--Please replace me-->
+    <section class="section container">
+      <h1> {{ title }} </h1>
+      <p>
+        The user's address you are viewing is {{ $route.params.userAddress }}
+      </p>
+      <NuxtLink to="/">
+        Back to home!
+      </NuxtLink>
+    </section>
+    <!--end of me-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserPage',
+  data () {
+    return {
+      title: 'Testing file name as dynamic route'
+    }
+  }
+}
+</script>
