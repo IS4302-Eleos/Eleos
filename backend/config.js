384b5f3532a5efd0eebc92db91b3242a9975c75f
3m0W33D <sky.t4y@gmail.com>
2022-03-13 20:15:01 +0800

    Fix Lint errors

--- backend/config.js
+++ backend/config.js
@@ -1,11 +1,11 @@
-import 'dotenv/config';
+import 'dotenv/config'
 
 export default {
-    app :{
-        port: process.env.API_PORT
-    },
-    db : {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-    }
+  app: {
+    port: process.env.API_PORT
+  },
+  db: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT
+  }
 }
--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,17 @@
-import Koa from 'koa';
-import Router from '@koa/router';
-import koaBody from 'koa-body';
-import config from './config.js';
+import Koa from 'koa'
+import Router from '@koa/router'
+import koaBody from 'koa-body'
+import config from './config.js'
 
-const app = new Koa();
-const router = new Router();
+const app = new Koa()
+const router = new Router()
 
-//routes to be developed
+// routes to be developed
 router.get('/', async (ctx) => {
-    ctx.body = JSON.stringify({'api_version': '1.0.0'});
+  ctx.body = JSON.stringify({ api_version: '1.0.0' })
 })
 
-app.use(koaBody());
-app.use(router.routes());
-app.use(router.allowedMethods());
-app.listen(config.app.port);
+app.use(koaBody())
+app.use(router.routes())
+app.use(router.allowedMethods())
+app.listen(config.app.port)
