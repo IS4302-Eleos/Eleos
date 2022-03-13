879937fa5889929551711123391170ad1d065cba
Kair0s3 <stianboon@gmail.com>
2022-03-13 15:04:59 +0800

    Add additional check for user addresses in `user/`pages

--- frontend/pages/user/index.vue
+++ frontend/pages/user/index.vue
@@ -7,6 +7,10 @@
 export default {
   name: 'UserAnonymousRouter',
   middleware ({ route, redirect, from }) {
+    const validUserAddress = /^[0-9a-fA-F]{1,3}$/.test(route.params.userAddress)
+    if (!validUserAddress) {
+      return redirect('/')
+    }
     if (/\/user[/]{0,1}$/.test(route.path)) {
       return redirect('/')
     }
