5d281afa5d60e484e65c12cb6a25ef8bf8eb7389
Kair0s3 <stianboon@gmail.com>
2022-03-13 15:01:11 +0800

    Add additional check for the address
    Reroutes the user back to '/' directory if the address is invalid. Will probably need to do for the user page as well.

--- frontend/pages/campaign/_campaignAddress/index.vue
+++ frontend/pages/campaign/_campaignAddress/index.vue
@@ -7,9 +7,15 @@
 export default {
   name: 'CampaignInfoAnonymousRouter',
   middleware ({ route, redirect, from }) {
-    if (/\/campaign[/][A-Fa-f0-9]{0,3}[/]{1}$/.test(route.path)) {
+    // Reroute to '/' directory if invalid address
+    const validAddress = /^[0-9a-fA-F]{1,3}$/.test(route.params.campaignAddress)
+    if (!validAddress) {
+      return redirect('/')
+    }
+
+    if (/\/campaign[/][0-9a-fA-F]{1,3}[/]{1,3}/.test(route.path)) {
       return redirect(route.path + 'info')
-    } else if (/\/campaign[/][A-Fa-f0-9]{0,3}$/.test(route.path)) {
+    } else if (/\/campaign[/][0-9a-fA-F]{1,3}/.test(route.path)) {
       return redirect(route.path + '/info')
     }
   }
