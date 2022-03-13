0531ed4cacaf763ee85fd61156ce7409abec160a
Kair0s3 <stianboon@gmail.com>
2022-03-13 10:49:09 +0800

    Added index.vue files for anonymous routing.
    Since, we only have a few site "sections", this would be able to do the work.

---
+++ frontend/pages/campaign/_campaignAddress/index.vue
@@ -0,0 +1,17 @@
+<!--Anonymous Router, please do not modify-->
+<template>
+  <NuxtChild />
+</template>
+
+<script>
+export default {
+  name: 'CampaignInfoAnonymousRouter',
+  middleware ({ route, redirect, from }) {
+    if (/\/campaign[/][A-Fa-f0-9]{0,3}[/]{1}$/.test(route.path)) {
+      return redirect(route.path + 'info')
+    } else if (/\/campaign[/][A-Fa-f0-9]{0,3}$/.test(route.path)) {
+      return redirect(route.path + '/info')
+    }
+  }
+}
+</script>
---
+++ frontend/pages/user/index.vue
@@ -0,0 +1,15 @@
+<!--Anonymous Router, please do not modify-->
+<template>
+  <NuxtChild />
+</template>
+
+<script>
+export default {
+  name: 'UserAnonymousRouter',
+  middleware ({ route, redirect, from }) {
+    if (/\/user[/]{0,1}$/.test(route.path)) {
+      return redirect('/')
+    }
+  }
+}
+</script>
