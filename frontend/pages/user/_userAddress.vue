ddf3bda189805a921cb4dd4c961318c3753a61b0
Kair0s3 <stianboon@gmail.com>
2022-03-13 17:34:25 +0800

    Add sample if else for future address validation

--- frontend/pages/user/_userAddress.vue
+++ frontend/pages/user/_userAddress.vue
@@ -3,8 +3,11 @@
     <!--Please replace me-->
     <section class="section container">
       <h1> {{ title }} </h1>
-      <p>
+      <p v-if="validUserAddress">
         The user's address you are viewing is {{ $route.params.userAddress }}
+      </p>
+      <p v-else>
+        Oops, this user address is not valid :(
       </p>
       <NuxtLink to="/">
         Back to home!
@@ -19,7 +22,8 @@ export default {
   name: 'UserPage',
   data () {
     return {
-      title: 'Testing file name as dynamic route'
+      title: 'Testing file name as dynamic route',
+      validUserAddress: true
     }
   }
 }
