04f622141db433396a33049eb3cc3b3cdb43bb38
Kair0s3 <stianboon@gmail.com>
2022-03-15 00:32:09 +0800

    Fix indents

--- blockchain/test/test_campaign.js
+++ blockchain/test/test_campaign.js
@@ -1,65 +1,66 @@
 const _deploy_contracts = require('../migrations/2_deploy_contracts');
-const truffleAssert = require('truffle-assertions');
+const truffleAssert = require('truffle-assertions')
 
-var assert = require('assert');
+const assert = require('assert')
 
-var campaign = artifacts.require("../contracts/Campaign.sol");
+const campaign = artifacts.require('../contracts/Campaign.sol')
 
-contract('Campaign', function(accounts) {
-    var testDonationAmount = "8000000000000000000";
-    var withdrawAmount = "2000000000000000000";
+contract('Campaign', function (accounts) {
+  const testDonationAmount = '8000000000000000000'
+  const withdrawAmount = '2000000000000000000'
 
-    before(async () => {
-        campaignInstance = await campaign.deployed();
-    });
+  let campaignInstance
+  before(async () => {
+    campaignInstance = await campaign.deployed()
+  })
 
-    console.log("Contracts Deployed")
+  console.log('Contracts Deployed')
 
-    it('Donating as a user', async () => {
-        let donation = await campaignInstance.donate({
-            from: accounts[1],
-            value: testDonationAmount
-        });
+  it('Donating as a user', async () => {
+    await campaignInstance.donate({
+      from: accounts[1],
+      value: testDonationAmount
+    })
 
-        var balance = await campaignInstance.getAvailableDonationBalance();
+    const balance = await campaignInstance.getAvailableDonationBalance()
 
-        var errorMessage = "There should be " + testDonationAmount + "...";
-        assert.equal(balance, testDonationAmount, errorMessage);
-    });
+    const errorMessage = 'There should be ' + testDonationAmount + '...'
+    assert.equal(balance, testDonationAmount, errorMessage)
+  })
 
-    // The beneficiary is accounts[3] as of test deployment.
-    it('Withdrawing as the beneficiary', async () => {
-        let withdraw = await campaignInstance.withdraw(withdrawAmount, {
-            from: accounts[3]
-        });
-        console.log(withdraw);
-        truffleAssert.eventEmitted(
-            withdraw,
-            'withdrawn',
-            null,
-            "The beneficiary should be able to withdraw from the campaign..."
-        );
-    });
+  // The beneficiary is accounts[3] as of test deployment.
+  it('Withdrawing as the beneficiary', async () => {
+    const withdraw = await campaignInstance.withdraw(withdrawAmount, {
+      from: accounts[3]
+    })
+    console.log(withdraw)
+    truffleAssert.eventEmitted(
+      withdraw,
+      'withdrawn',
+      null,
+      'The beneficiary should be able to withdraw from the campaign...'
+    )
+  })
 
-    // Campaign owner is accounts[4] as of test deployment.
-    it('Withdrawing as campaign owner for beneficiary', async () => {
-        let withdraw = await campaignInstance.withdraw(withdrawAmount, {
-            from: accounts[4]
-        });
-        truffleAssert.eventEmitted(
-            withdraw,
-            'withdrawn',
-            null,
-            "The campaign owner should be able to initiate the withdraw to beneficiary..."
-         );
-    });
+  // Campaign owner is accounts[4] as of test deployment.
+  it('Withdrawing as campaign owner for beneficiary', async () => {
+    const withdraw = await campaignInstance.withdraw(withdrawAmount, {
+      from: accounts[4]
+    })
+    truffleAssert.eventEmitted(
+      withdraw,
+      'withdrawn',
+      null,
+      'The campaign owner should be able to initiate the withdraw to beneficiary...'
+    )
+  })
 
-    it('Withdrawing as other user', async () => {
-        await truffleAssert.fails(
-            campaignInstance.withdraw(withdrawAmount, {
-                from: accounts[2]
-            }),
-            truffleAssert.ErrorType.REVERT
-        );
-    });
-});
+  it('Withdrawing as other user', async () => {
+    await truffleAssert.fails(
+      campaignInstance.withdraw(withdrawAmount, {
+        from: accounts[2]
+      }),
+      truffleAssert.ErrorType.REVERT
+    )
+  })
+})
