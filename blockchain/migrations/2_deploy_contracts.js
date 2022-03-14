607aef0ae22a0ddc8b0f1f2f7272cc6e028a23eb
Kair0s3 <stianboon@gmail.com>
2022-03-15 00:32:25 +0800

    Fix indents 2.0

--- blockchain/migrations/2_deploy_contracts.js
+++ blockchain/migrations/2_deploy_contracts.js
@@ -1,13 +1,13 @@
-const Campaign = artifacts.require("Campaign");
+const Campaign = artifacts.require('Campaign')
 
 module.exports = (deployer, network, accounts) => {
-    deployer.deploy(
-        Campaign,
-        "TestCampaign1",
-        "http://test.org",
-        1647270580,
-        accounts[3],
-        accounts[4],
-        "10000000000000000000"
-    );
+  deployer.deploy(
+    Campaign,
+    'TestCampaign1',
+    'http://test.org',
+    1647270580,
+    accounts[3],
+    accounts[4],
+    '10000000000000000000'
+  )
 }
