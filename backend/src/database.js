7b77532b8491fb9fbb6017f968700d203e17bed3
3m0W33D <sky.t4y@gmail.com>
2022-03-14 23:44:15 +0800

    Fixed minor changes

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
-import config from './config.js'
+import config from '../config.js'
 
 export default () => {
   mongoose.connect(`mongodb://${config.db.host}:${config.db.port}/${config.db.name}`, {
--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -51,6 +51,5 @@ export default {
   publicRuntimeConfig: {
   },
   privateRuntimeConfig: {
-
   }
 }
