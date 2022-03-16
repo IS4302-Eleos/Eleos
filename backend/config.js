30d35c72bbc2bfadcf04543d5d9a579489fe0be1
3m0W33D <sky.t4y@gmail.com>
2022-03-16 20:10:48 +0800

    Update fallback for configs

--- backend/config.js
+++ backend/config.js
@@ -2,14 +2,14 @@ import 'dotenv/config'
 
 export default {
   app: {
-    port: process.env.API_PORT
+    port: process.env.API_PORT|| 8000
   },
   db: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    name: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    pass: process.env.DB_PASS
+    host: process.env.DB_HOST || localhost,
+    port: process.env.DB_PORT || 27017,
+    name: process.env.DB_NAME || 'Eleos',
+    user: process.env.DB_USER || 'test',
+    pass: process.env.DB_PASS || 'test'
   },
   graphiql: process.env.GRAPHIQL_ENABLED || false
 }
