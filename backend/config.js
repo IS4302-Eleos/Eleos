d9ad6b57a2f898075641265d67557d0fd591c97d
3m0W33D <sky.t4y@gmail.com>
2022-03-16 20:12:16 +0800

    Update configs

--- backend/config.js
+++ backend/config.js
@@ -2,10 +2,10 @@ import 'dotenv/config'
 
 export default {
   app: {
-    port: process.env.API_PORT|| 8000
+    port: process.env.API_PORT || 8000
   },
   db: {
-    host: process.env.DB_HOST || localhost,
+    host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 27017,
     name: process.env.DB_NAME || 'Eleos',
     user: process.env.DB_USER || 'test',
