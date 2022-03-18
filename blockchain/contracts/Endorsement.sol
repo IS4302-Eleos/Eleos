2466f8dbd47c5e08b9f44eb78cb582832905c3de
arraysius <16810847+arraysius@users.noreply.github.com>
2022-03-19 02:10:31 +0800

    Add minimal Endorsement.sol
    Add AddressSet library by Rob Hitchens (https://github.com/rob-Hitchens/SetTypes)
    Add Endorsement.sol

---
+++ blockchain/contracts/Endorsement.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.5.0;
+import "./AddressSet.sol";
+
+contract Endorsement {
+    using AddressSet for AddressSet.Set;
+
+    mapping(address => AddressSet.Set) endorserSet; // Accounts that are endorsing this user
+    mapping(address => AddressSet.Set) endorseeSet; // Accounts that this user are endorsing
+
+    // Endorse a user
+    function endorse(address endorsee) public {
+        endorserSet[endorsee].insert(msg.sender);
+        endorseeSet[msg.sender].insert(endorsee);
+    }
+
+    // Retract a user's endorsement
+    function retract(address endorsee) public {
+        endorserSet[endorsee].remove(msg.sender);
+        endorseeSet[msg.sender].remove(endorsee);
+    }
+
+    function getEndorsers(address user) public view returns (address[] memory) {
+        return endorserSet[user].keyList;
+    }
+
+    function getEndorserCount(address user) public view returns (uint256) {
+        return endorserSet[user].count();
+    }
+
+    function getEndorsees(address user) public view returns (address[] memory) {
+        return endorseeSet[user].keyList;
+    }
+
+    function getEndorseeCount(address user) public view returns (uint256) {
+        return endorseeSet[user].count();
+    }
+}
