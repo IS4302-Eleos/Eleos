527ccedc498e271a7536e8fe219ae631a5054a73
arraysius <16810847+arraysius@users.noreply.github.com>
2022-03-21 01:38:15 +0800

    Pass rootState to setCampaigns to use web3

--- frontend/store/api.js
+++ frontend/store/api.js
@@ -6,14 +6,25 @@ export const state = () => ({
 
 export const mutations = {
   // Setting campaigns to state variables
-  setCampaigns (state, campaigns) {
+  setCampaigns (state, payload) {
+    const rootState = payload.rootState
+    const campaigns = payload.campaigns
+
     campaigns.forEach((campaign) => {
+      // Set temp donation value
       if (!campaign.donations) {
         campaign.donations = 0
       }
 
       // Convert date string to date object
       campaign.endTimestamp = new Date(campaign.endTimestamp)
+
+      // Convert target donation amount to eth
+      campaign.targetDonationAmount = Number(
+        rootState.web3.utils.fromWei(
+          campaign.targetDonationAmount.toString(),
+          'ether'
+        ))
     })
     state.campaigns = campaigns
   }
@@ -21,7 +32,7 @@ export const mutations = {
 
 export const actions = {
   // Getting all the campaigns from the database
-  async getCampaigns ({ commit }) {
+  async getCampaigns (context) {
     const query = {
       query: {
         campaigns: {
@@ -36,12 +47,14 @@ export const actions = {
       }
     }
     const res = await this.$graphql.default.request(jsonToGraphQLQuery(query))
-    commit('setCampaigns', res.campaigns)
+    context.commit('setCampaigns', {
+      rootState: context.rootState, campaigns: res.campaigns
+    })
     return res.campaigns
   },
   // Getting the campaign from the database filtered by a date
   // Note this function expects a nodejs date object
-  async getCampaignsByDate ({ commit }, date) {
+  async getCampaignsByDate (context, date) {
     const query = {
       query: {
         campaigns: {
@@ -65,7 +78,9 @@ export const actions = {
       }
     }
     const res = await this.$graphql.default.request(jsonToGraphQLQuery(query))
-    commit('setCampaigns', res.campaigns)
+    context.commit('setCampaigns', {
+      rootState: context.rootState, campaigns: res.campaigns
+    })
     return res.campaigns
   }
 }
