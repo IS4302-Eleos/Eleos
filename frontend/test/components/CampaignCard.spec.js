2f687bfc43965117d3303cd317e959b1278c5553
kelvneo <2332196+kelvneo@users.noreply.github.com>
2022-03-22 22:37:47 +0800

    Add temporary test case

---
+++ frontend/test/components/CampaignCard.spec.js
@@ -0,0 +1,22 @@
+import { shallowMount } from '@vue/test-utils'
+import CampaignCard from '@/components/CampaignCard.vue'
+
+describe('CampaignCard', () => {
+  test('is a Vue instance', () => {
+    const wrapper = shallowMount(CampaignCard, {
+      props: {
+        address: '0x17ee2FbBA2C47657Bd11d5378a26aaD1e1A0eaA4',
+        title: 'Testing'
+      },
+      mocks: {
+        $store: {
+          state: {
+            isConnected: false
+          }
+        }
+      },
+      stubs: ['nuxt-link', 'b-skeleton']
+    })
+    expect(wrapper.vm).toBeTruthy()
+  })
+})
