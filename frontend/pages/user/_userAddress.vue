0ef923eda5c0efb0e6411531832c070700eefbce
Kair0s3 <stianboon@gmail.com>
2022-03-25 06:04:59 -0700

    Base User Page UI
    Base template for User page

--- frontend/pages/user/_userAddress.vue
+++ frontend/pages/user/_userAddress.vue
@@ -2,16 +2,63 @@
   <div>
     <!--Please replace me-->
     <section class="section container">
-      <h1> {{ title }} </h1>
-      <p v-if="validUserAddress">
-        The user's address you are viewing is {{ $route.params.userAddress }}
-      </p>
-      <p v-else>
-        Oops, this user address is not valid :(
-      </p>
-      <NuxtLink to="/">
-        Back to home!
-      </NuxtLink>
+      <div class="container">
+        <div class="card">
+          <div class="hero is-success">
+            <div class="hero-body">
+              <figure class="image is-128x128">
+                <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png" alt="Placeholder image">
+              </figure>
+              <p class="subtitle mt-6">
+                {{ $route.params.userAddress }}
+              </p>
+            </div>
+          </div>
+          <div class="card-content">
+            <div class="media">
+              <div class="media-content">
+                <div class="level">
+                  <div class="level-item has-text-centered">
+                    <div>
+                      <p class="heading">
+                        Reputation
+                      </p>
+                      <p class="title">123</p>
+                    </div>
+                  </div>
+                  <div class="level-item has-text-centered">
+                    <div>
+                      <p class="heading">
+                        No. of Donations Made
+                      </p>
+                      <p class="title">123</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <b-tabs position="is-centered" class="block">
+              <b-tab-item label="Donation Records">
+                <div class="media">
+                  <div class="media-left">
+                    Donated to Campaign at <NuxtLink :to="`/campaign/${donor}`">
+                      { 0xDEAD }
+                    </NuxtLink>
+                  </div>
+                </div>
+              </b-tab-item>
+
+              <b-tab-item label="Benefitted Campaigns">
+                <div class="media">
+                  <div class="media-left">
+                    Added as beneficiary to Campaign at {0x505}
+                  </div>
+                </div>
+              </b-tab-item>
+            </b-tabs>
+          </div>
+        </div>
+      </div>
     </section>
     <!--end of me-->
   </div>
